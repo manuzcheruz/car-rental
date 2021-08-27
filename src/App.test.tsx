72ec6913c4f0eaa5087d1ff1260c80c8d8099ff0
manuzcheruz <kipkemoi.emanuel@gmail.com>
2021-08-27 14:29:07 +0300

    using jest for testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should have a learn react text', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
